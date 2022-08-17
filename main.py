296a4add06081c619d0f7bda335bf29f7bd77b66
nadyanf <75020430+nadyanf@users.noreply.github.com>
2022-08-17 11:51:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ check_security_key=(security_key in df['Token'].astype(str).unique())
 if check_security_key is False:
     st.error("The security key: "+security_key+" is invalid.")
 else:
-  if check_security_key if True:
+  if check_security_key is True:
     st.text("Name: Nadya Alfardan "+ "ID: 12346")
     
         
